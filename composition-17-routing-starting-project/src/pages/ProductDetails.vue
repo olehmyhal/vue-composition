dc8a7551b034ab7301b6c3d8100b367d5ef51dc6
olehmyhal <olehmyhal.pers@gmail.com>
2022-04-15 09:39:55 +0300

    composition examples

---
+++ composition-17-routing-starting-project/src/pages/ProductDetails.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    <h3>${{ price}}</h3>
+    <p>{{ description}}</p>
+  </section>
+</template>
+
+<script>
+import { inject } from 'vue';
+import { useRoute } from 'vue-router'
+
+export default {
+  props: ['pid'],
+  setup(props) {
+    const products = inject('products')
+
+    const route = useRoute()
+    console.log(route)
+
+    const selectedProduct = products.value.find(product => product.id === props.pid)
+    const { title, price, description } = selectedProduct
+
+    return { title, price, description };
+  },
+};
+</script>
+
+
+<style scoped>
+section {
+  margin: 3rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ composition-assignment-1-problem/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>My Course Goal</h2>
+    <p>{{ mainGoal }}</p>
+    <!-- Task 1: Output your main course goal with help of the composition API -->
+    <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+    <h3>OUTPUT COURSE GOAL</h3>
+    <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+    <button @click="toggleText">{{ isShownText ? 'Show' : 'Hide' }}</button>
+    <!-- Task 3: Manage data in three ways -->
+    <!-- => Separate refs -->
+    <!-- => Ref Object -->
+    <!-- => Reactive Object -->
+    <!-- Task 4: Also solve the assignment with the Options API -->
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup(){
+    const mainGoal = ref('To become a google software developer')
+    const isShownText = ref(true)
+
+    const toggleText = () => isShownText.value = !isShownText.value
+
+    return { mainGoal, toggleText, isShownText }
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
